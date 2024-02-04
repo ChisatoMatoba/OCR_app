1990ce9eb98446d2d15baae063f78753c87f3471
Chisato Matoba <chisato.matoba@luxiar.com>
2024-02-04 16:27:00 +0900

    book登録

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path, notice: '登録が完了しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:book_name, :status)
+  end
 end
