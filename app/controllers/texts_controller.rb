560a9e6eb1186430cb65b6bcb1a1f9486d255f43
Chisato Matoba <chisato.matoba@luxiar.com>
2024-03-17 21:21:34 +0900

    変換できた！

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -5,18 +5,20 @@ class TextsController < ApplicationController
   end
 
   def create
-    @text = Text.new(text_params)
     @image = Image.find(params[:image_id])
 
-    image_blob = @image.image.blob
-    image_url = Rails.application.routes.url_helpers.rails_blob_url(image_blob, only_path: true)
+    # Active Storageから画像を取得して一時ファイルに保存
+    download_blob_to_tempfile(@image.image.blob) do |tempfile|
+      # RTesseractを使って一時ファイルからテキストを抽出
+      image_rtesseract = RTesseract.new(tempfile.path, lang: 'jpn')
+      extracted_text = image_rtesseract.to_s
+      @text = Text.new(content: extracted_text, book_id: params[:book_id], image_id: params[:image_id])
 
-    image_rtesseract = RTesseract.new(image_url, lang: 'jpn')
-    @text.content = image_rtesseract.to_s
-    if @text.save
-      redirect_to book_text_path(text), notice: '文字変換が完了しました'
-    else
-      render :new, status: :unprocessable_entity
+      if @text.save
+        redirect_to book_path(@text.book_id), notice: '文字変換が完了しました'
+      else
+        redirect_to book_path(@text.book_id), status: :unprocessable_entity
+      end
     end
   end
 
@@ -24,5 +26,17 @@ class TextsController < ApplicationController
 
   def text_params
     params.require(:text).permit(:content, :fixed_content).merge(book_id: params[:book_id], image_id: params[:image_id])
+  end
+
+  # Active StorageのBlobを一時ファイルとしてダウンロード
+  def download_blob_to_tempfile(blob)
+    tempfile = Tempfile.new([blob.filename.to_s, blob.filename.extension_with_delimiter])
+    tempfile.binmode
+    tempfile.write(blob.download)
+    tempfile.rewind
+    yield(tempfile)
+  ensure
+    tempfile.close
+    tempfile.unlink # 一時ファイルを削除
   end
 end
